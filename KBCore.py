6409aaecc23d8702b695e8e162015882a696d45f
TeeSHMN <daniel.sapozhnykov@gmail.com>
2018-12-19 16:28:42 +1100

    Add allban, allkick, allunban

--- KBCore.py
+++ KBCore.py
@@ -60,8 +60,8 @@ async def on_guild_join(guild):
         embed.set_footer(text=KAEBOT_VERSION)
         embed.set_thumbnail(url="https://cdn.pbrd.co/images/HGYlRKR.png")
         embed.add_field(name="Hey there, I'm KaeBot!",
-                        value=f"Hi! I'm KaeBot, a **discord.py** bot written by TSHMN (and aliases).\n"
-                              "I currently have {len(bot.commands))} commands available to use; type 'kae help' to see them!\n"
+                        value="Hi! I'm KaeBot, a **discord.py** bot written by TSHMN (and aliases).\n"
+                              f"I currently have {len(bot.commands)} commands available to use; type 'kae help' to see them!\n"
                               "If you want to change my prefix, use 'kae prefix add'.\n Have fun!",
                         inline=False)
         await guild.system_channel.send(embed=embed)
@@ -207,7 +207,7 @@ class Administrator:
     @commands.command(name="prune", brief="Prunes members from the server.",
                       description="Prune members from the server who have been inactive for a specified number of days"
                                   " (default: 5) and who have no roles.")
-    async def prune(self, ctx, daysinactive: int=5, reason: str=None):
+    async def prune(self, ctx, daysinactive: int=5):
         if ctx.author.guild_permissions.administrator:
             estimate = await ctx.guild.estimate_pruned_members(days=daysinactive)
             if not estimate == 0:
@@ -218,7 +218,7 @@ class Administrator:
                                       f" {daysinactive} days.")
             message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
             if message.content == "y":
-                await ctx.guild.prune_members(days=daysinactive, reason=reason)
+                await ctx.guild.prune_members(days=daysinactive, reason="Pruned due to inactivity.")
                 await ctx.send(f"Purged {estimate} members.")
             elif message.content == "n":
                 await ctx.send("Prune cancelled.")
@@ -226,6 +226,85 @@ class Administrator:
                 await ctx.send("Invalid input. Presuming `n` response; cancelling prune.")
         else:
             await ctx.send("You lack the following permissions to do this:\n```css\nAdministrator\n```")
+
+    @commands.command(name="allkick", brief="Kicks everyone in the server. Use this wisely.",
+                      description="Kicks everyone in the server. Skips kickable members.\n"
+                                  "Restricted to Administrator.")
+    @commands.bot_has_permissions(kick_members=True)
+    @commands.has_permissions(kick_members=True)
+    async def allkick(self, ctx):
+        message = await ctx.send("\U00002757WARNING\U00002757 This command will **kick everyone** in the server "
+                                 "except unkickable members. Are you sure you want to proceed?")
+        await message.add_reaction("\U00002705")
+        await message.add_reaction("\U0000274e")
+        try:
+            reaction, user = await bot.wait_for("reaction_add",
+                                                check=lambda r, u: r.message.id == message.id and u == ctx.author and str(r.emoji) in ["✅", "❎"],
+                                                timeout=10)
+        except asyncio.TimeoutError:
+            return await ctx.send("Operation cancelled due to lack of response.")
+        if str(reaction.emoji) == "✅":
+            await ctx.send("\U00002699Processing...\U00002699")
+            for member in ctx.guild.members:
+                try:
+                    await ctx.guild.kick(member)
+                except discord.Forbidden:
+                    await ctx.send(f"Skipping member {member} due to discord.Forbidden exception.")
+            await ctx.send("Allkick complete.")
+        elif str(reaction.emoji) == "❎":
+            return await ctx.send("Allkick cancelled.")
+
+    @commands.command(name="allban", brief="Bans everyone in the server. Use this wisely.",
+                      description="Bans everyone in the server. Skips unbannable members.\n"
+                                  "Restricted to Administrator.")
+    @commands.bot_has_permissions(ban_members=True)
+    @commands.has_permissions(ban_members=True)
+    async def allban(self, ctx):
+        message = await ctx.send("\U00002757WARNING\U00002757 This command will **ban everyone** in the server "
+                                 "except unbannable members. Are you sure you want to proceed?")
+        await message.add_reaction("\U00002705")
+        await message.add_reaction("\U0000274e")
+        try:
+            reaction, user = await bot.wait_for("reaction_add",
+                                                check=lambda r, u: r.message.id == message.id and u == ctx.author and str(r.emoji) in ["✅", "❎"],
+                                                timeout=10)
+        except asyncio.TimeoutError:
+            return await ctx.send("Operation cancelled due to lack of response.")
+        if str(reaction.emoji) == "✅":
+            await ctx.send("\U00002699Processing...\U00002699")
+            for member in ctx.guild.members:
+                try:
+                    await ctx.guild.ban(member)
+                except discord.Forbidden:
+                    await ctx.send(f"Skipping member {member} due to discord.Forbidden exception.")
+            await ctx.send("Allban complete.")
+        elif str(reaction.emoji) == "❎":
+            return await ctx.send("Allban cancelled.")
+
+    @commands.command(name="allunban", brief="Unbans everyone who has been banned from the server.",
+                      description="Unbans everyone who is currently banned from the server.\n"
+                                  "Restricted to Administrator.")
+    @commands.bot_has_permissions(ban_members=True)
+    @commands.has_permissions(ban_members=True)
+    async def allunban(self, ctx):
+        message = await ctx.send("\U00002757WARNING\U00002757 This command will **unban everyone** in the server "
+                                 ". Are you sure you want to proceed?")
+        await message.add_reaction("\U00002705")
+        await message.add_reaction("\U0000274e")
+        try:
+            reaction, user = await bot.wait_for("reaction_add",
+                                                check=lambda r, u: r.message.id == message.id and u == ctx.author and str(r.emoji) in ["✅", "❎"],
+                                                timeout=10)
+        except asyncio.TimeoutError:
+            return await ctx.send("Operation cancelled due to lack of response.")
+        if str(reaction.emoji) == "✅":
+            await ctx.send("\U00002699Processing...\U00002699")
+            banlist = await ctx.guild.bans()
+            for banned in banlist:
+                await ctx.guild.unban(banned[1])
+            await ctx.send("Allunban complete.")
+        elif str(reaction.emoji) == "❎":
+            return await ctx.send("Allunban cancelled.")
 
 
 class Moderator:
