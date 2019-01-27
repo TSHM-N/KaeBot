3034fb34e2bff5c97451d22fac56725184230a7d
TeeSHMN <daniel.sapozhnykov@gmail.com>
2019-01-27 16:01:54 +1100

    Rewrite seasonal commands for sake of legibility

--- cogs/seasonal.py
+++ cogs/seasonal.py
@@ -14,29 +14,13 @@ class Seasonal:
     )
     async def spooky(self, ctx):
         if datetime.datetime.today().month == 10:
-            try:
-                if not ctx.message.author.nick.endswith("\U0001f383"):
-                    if ctx.message.author.nick.endswith("\U0001f384"):  # If already ends with Christmas emoji
-                        changednick = ctx.message.author.nick[:-1] + " \U0001f383"
-                    else:
-                        changednick = ctx.message.author.nick + " \U0001f383"
-                    await ctx.message.author.edit(nick=changednick)
-                    await ctx.send(f"Your nickname is now '{changednick}'.")
-                else:
-                    await ctx.send("Your nickname is already spooky!")
-
-            except AttributeError:  # except if the person has no nick, because endswith is null
-                if not ctx.message.author.name.endswith("\U0001f383"):
-                    if ctx.message.author.name.endswith("\U0001f384"):  # If already ends with Christmas emoji
-                        changednick = ctx.message.author.name[:-1] + " \U0001f383"
-                    else:
-                        changednick = ctx.message.author.name + " \U0001f383"
-                    await ctx.message.author.edit(nick=changednick)
-                    await ctx.send(f"Your nickname is now '{changednick}'.")
-                else:
-                    await ctx.send("Your nickname is already spooky!")
+            if ctx.message.author.display_name.endswith("\U0001f383"):
+                await ctx.send("Your nickname is already spooky!")
+            else:
+                await ctx.message.author.edit(nick=ctx.message.author.display_name + " \U0001f383")
+                await ctx.send(f"Your nickname is now '{ctx.author.display_name}'.")
         else:
-            await ctx.send("It's not October, you can't use this yet!")
+            await ctx.send("You can't use this yet...")
 
     @commands.command(
         name="christmas",
@@ -45,29 +29,13 @@ class Seasonal:
     )
     async def christmas(self, ctx):
         if datetime.datetime.today().month == 12:
-            try:
-                if not ctx.message.author.nick.endswith("\U0001f384"):
-                    if ctx.message.author.nick.endswith("\U0001f383"):  # If already ends with Spooky emoji
-                        changednick = ctx.message.author.nick[:-1] + " \U0001f384"
-                    else:
-                        changednick = ctx.message.author.nick + " \U0001f384"
-                    await ctx.message.author.edit(nick=changednick)
-                    await ctx.send(f"Your nickname is now '{changednick}'.")
-                else:
-                    await ctx.send("Your nickname is already Christmassy!")
-
-            except AttributeError:  # except if the person has no nick, because endswith is null
-                if not ctx.message.author.name.endswith("\U0001f384"):
-                    if ctx.message.author.name.endswith("\U0001f383"):  # If already ends with Spooky emoji
-                        changednick = ctx.message.author.name[:-1] + " \U0001f384"
-                    else:
-                        changednick = ctx.message.author.name + " \U0001f384"
-                    await ctx.message.author.edit(nick=changednick)
-                    await ctx.send(f"Your nickname is now '{changednick}'.")
-                else:
-                    await ctx.send("Your nickname is already Christmassy!")
+            if ctx.message.author.display_name.endswith("\U0001f384"):
+                await ctx.send("Your nickname is already Christmassy!")
+            else:
+                await ctx.message.author.edit(nick=ctx.message.author.display_name + " \U0001f384")
+                await ctx.send(f"Your nickname is now '{ctx.author.display_name}'.")
         else:
-            await ctx.send("It's not December, you can't use this yet!")
+            await ctx.send("You can't use this yet...")
 
 
 def setup(bot):
